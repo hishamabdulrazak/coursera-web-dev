c028efe38bb2f104288f0c112e4d0a41c2553a63
hishamabdulrazak <34984@yenepoya.edu.in>
2025-04-23 23:22:19 +0530

    Add files via upload

---
+++ module 4/helloSpeaker.js
@@ -0,0 +1,8 @@
+(function (window) {
+    var helloSpeaker = {};
+    helloSpeaker.speak = function (name) {
+      console.log("Hello " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+  
+  })(window);
